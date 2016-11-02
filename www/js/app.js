75bea8db8c1d2fc8efe7b4b6d7607e1b67d9e936
Brandon Woo <bmw.woo@gmail.com>
2016-11-02 14:39:38 -0700

    Form submitting works for local instance

--- www/js/app.js
+++ www/js/app.js
@@ -3,18 +3,31 @@ $(document).ready(function($) {
 });
 
 function authSpotify() {
-    $.get("/authspotify").done(function(data){
-        window.location = data;
-        //console.log(data);
-    });
+  $.get("/authspotify").done(function(data){
+      window.location = data;
+      //console.log(data);
+  });
 }
 
 function showCreatePlaylistModule() {
   $("body").load("overlay.html",function(){
     $(this).clone().appendTo("body").remove();
+    $('#createPlaylistForm').submit(function () {
+      createPlaylist();
+    });
   });
+
 }
 
-function createPlaylist(usrnm, pswd, secretCode, activeTime) {
-  console.log($('#usrnm'));
+function createPlaylist() {
+  // get all the inputs into an array.
+  var $inputs = $('#createPlaylistForm :input');
+
+  // not sure if you wanted this, but I thought I'd add it.
+  // get an associative array of just the values.
+  var values = {};
+  $inputs.each(function() {
+      values[this.name] = $(this).val();
+  });
+  console.log(values);
 }
