ba284052c19286a7a7fa36400011f72f571b5067
Brandon Woo <bmw.woo@gmail.com>
2016-11-02 13:04:30 -0700

    Basic form

--- handler.go
+++ handler.go
@@ -87,7 +87,8 @@ func Dashboard(rw http.ResponseWriter, r *http.Request) {
         ProfilePic:  client_info.User.Images[2].URL,
     }*/
 
-    fmt.Fprint(rw, client_info)
+    body, _ := ioutil.ReadFile("www/dashboard.html")
+    fmt.Fprint(rw, string(body))
     // the client can now be used to make authenticated requests
 }
 
