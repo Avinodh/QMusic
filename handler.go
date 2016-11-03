e13fef719abdfd10e9e68c5c30ba190bf0306cd9
Avinodh <Ashvin@Ashvins-MacBook-Pro.local>
2016-11-02 19:18:08 -0700

    Updated spotify auth code to extract auth info and token into separate object

--- handler.go
+++ handler.go
@@ -1,94 +1,91 @@
-
 package main
 
 /************** Functions that handle all routes/ Defines our API *************/
 
 import (
-    "fmt"
-    //"github.com/gorilla/mux"
-    "crypto/rand"
-    "encoding/base64"
-    "github.com/gorilla/sessions"
-    "github.com/zmb3/spotify"
-    "io/ioutil"
-    "net/http"
-    "os"
+	"fmt"
+	_"github.com/gorilla/mux"
+	"bytes"
+	"crypto/rand"
+	"encoding/base64"
+	"encoding/gob"
+	"encoding/json"
+	"github.com/gorilla/sessions"
+	"github.com/zmb3/spotify"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"strings"
+	_"net/url"
 )
 
 var (
-    store       = sessions.NewCookieStore([]byte(os.Getenv("COOKIE_STORE_AUTHENTICATION_KEY")), []byte(os.Getenv("COOKIE_STORE_ENCRYPTION_KEY")))
-    redirectURL = os.Getenv("REDIRECT_URL")
-    auth        = spotify.NewAuthenticator(redirectURL, spotify.ScopeUserReadPrivate)
+	store       = sessions.NewCookieStore([]byte(os.Getenv("COOKIE_STORE_AUTHENTICATION_KEY")), []byte(os.Getenv("COOKIE_STORE_ENCRYPTION_KEY")))
+	redirectURL = os.Getenv("REDIRECT_URL")
+	auth        = spotify.NewAuthenticator(redirectURL, spotify.ScopeUserReadPrivate)
 )
 
 func init() {
-    store.Options = &sessions.Options{
-        Path:     "/",
-        MaxAge:   3600 * 8, // 8 hours
-        HttpOnly: true,
-    }
+	store.Options = &sessions.Options{
+		Path: "/",
+		//MaxAge:   3600 * 8, // 8 hours
+		HttpOnly: true,
+	}
 }
 
 func Index(rw http.ResponseWriter, r *http.Request) {
-    body, _ := ioutil.ReadFile("www/index.html")
-    fmt.Fprint(rw, string(body))
+	body, _ := ioutil.ReadFile("www/index.html")
+	fmt.Fprint(rw, string(body))
 }
 
 func AuthorizeSpotify(rw http.ResponseWriter, r *http.Request) {
-    // Setting GET request query parameters
-    /*v := url.Values{}
-      v.Set("client_id", client_spotify_id)
-      v.Set("response_type", "code")
-      v.Set("redirect_uri", "http://qmusicapp.herokuapp.com/dashboard")
+	// Generating Session Token
+	session, _ := store.Get(r, "UserAuthStore")
 
-      queryString := fmt.Sprintf("https://accounts.spotify.com/authorize/?%s", v.Encode())
+	session_token, _ := GenerateRandomString(32)
 
-      resp, _ := http.Get(queryString)
-      fmt.Fprint(rw, resp)*/
+	// Setting Session Token
+	session.Values["session_token"] = &session_token
+	err := sessions.Save(r, rw)
+	if err != nil {
+		fmt.Fprint(rw, "Session not saved! ", err)
+	}
 
-    // Generating Session Token
-    session, _ := store.Get(r, "UserAuthStore")
+	url := auth.AuthURL(session_token)
 
-    session_token, _ := GenerateRandomString(32)
-
-    // Setting Session Token
-    session.Values["session_token"] = &session_token
-    err := sessions.Save(r, rw)
-    if err != nil {
-        fmt.Fprint(rw, "Session not saved! ", err)
-    }
-
-    // get the user to this URL - how you do that is up to you
-    // you should specify a unique state string to identify the session
-    url := auth.AuthURL(session_token)
-
-    fmt.Fprint(rw, url)
+	fmt.Fprint(rw, url)
 }
 
 func Dashboard(rw http.ResponseWriter, r *http.Request) {
-    session, _ := store.Get(r, "UserAuthStore")
-    session_token := fmt.Sprint(session.Values["session_token"])
+	session, _ := store.Get(r, "UserAuthStore")
+	//session_token := fmt.Sprint(session.Values["session_token"])
 
-    // use the same state string here that you used to generate the URL
-    token, err := auth.Token(session_token, r)
-    if err != nil {
-        http.Error(rw, err.Error(), http.StatusNotFound)
-        return
-    }
-    // create a client using the specified token
-    client := auth.NewClient(token)
+	// use the same state string here that you used to generate the URL
+	r.ParseForm()
+	code := r.FormValue("code")
 
-    client_info, _ := client.CurrentUser()
+	authTokenUrl := fmt.Sprintf("https://accounts.spotify.com/api/token")
 
-   // Extract user data from spotify client and populate Spotify User model
-    /*user := Spotify_User{
-        Id:          client_info.User.ID,
-        DisplayName: client_info.User.DisplayName,
-        ProfilePic:  client_info.User.Images[2].URL,
-    }*/
+	
+	// POST request to fetch Access Token
+	params := fmt.Sprintf("grant_type=authorization_code&code=%s&redirect_uri=%s&client_id=%s&client_secret=%s", code, redirectURL, os.Getenv("SPOTIFY_ID"), os.Getenv("SPOTIFY_SECRET"))
+	body := strings.NewReader(params)
+	req, err := http.NewRequest("POST", authTokenUrl, body)
+	if err != nil {
+		// handle err
+	}
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	resp, _ := http.DefaultClient.Do(req)
 
-    fmt.Fprint(rw, client_info)
-    // the client can now be used to make authenticated requests
+	respBody, _ := ioutil.ReadAll(resp.Body)
+
+	err = json.Unmarshal(respBody, &Spotify_Auth_Object)
+
+	session.Values["spotify_access_token"] = &Spotify_Auth_Object.AccessToken
+
+	// Spotify_Auth_Object.AccessToken now contains the access token required for all API calls
+	fmt.Fprint(rw, Spotify_Auth_Object.AccessToken)
+	// the client can now be used to make authenticated requests
 }
 
 /************** BEGIN SECTION: HELPER FUNCTIONS *************/
@@ -98,14 +95,14 @@ func Dashboard(rw http.ResponseWriter, r *http.Request) {
 // number generator fails to function correctly, in which
 // case the caller should not continue.
 func GenerateRandomBytes(n int) ([]byte, error) {
-    b := make([]byte, n)
-    _, err := rand.Read(b)
-    // Note that err == nil only if we read len(b) bytes.
-    if err != nil {
-        return nil, err
-    }
+	b := make([]byte, n)
+	_, err := rand.Read(b)
+	// Note that err == nil only if we read len(b) bytes.
+	if err != nil {
+		return nil, err
+	}
 
-    return b, nil
+	return b, nil
 }
 
 // GenerateRandomString returns a URL-safe, base64 encoded
@@ -114,8 +111,31 @@ func GenerateRandomBytes(n int) ([]byte, error) {
 // number generator fails to function correctly, in which
 // case the caller should not continue.
 func GenerateRandomString(s int) (string, error) {
-    b, err := GenerateRandomBytes(s)
-    return base64.URLEncoding.EncodeToString(b), err
+	b, err := GenerateRandomBytes(s)
+	return base64.URLEncoding.EncodeToString(b), err
+}
+
+func GetBytes(key interface{}) ([]byte, error) {
+	var buf bytes.Buffer
+	enc := gob.NewEncoder(&buf)
+	err := enc.Encode(key)
+	if err != nil {
+		return nil, err
+	}
+	return buf.Bytes(), nil
 }
 
 /************** END SECTION: HELPER FUNCTIONS *************/
+
+/*Fetch Playlists API endpoint */
+	/*httpClient := &http.Client{}
+
+	  getPlaylistsUrl := fmt.Sprint("https://api.spotify.com/v1/users/12124324757/playlists")
+	  authHeader := fmt.Sprintf("Bearer %s", "BQCdp2D2ocHOyYPdP-1CDlaAsH6uKn3TMhoCPpJFpwWPufArd_01VBjXWoevKmuqqoala4yrFbWAW15VbGM4uA0yHhzmTf0_IJVz-VA5iVO-cKxwF4sHGr23osmxGg7_E9MBCPUwQbmEmNrDeDwBnrWO_nd8p3tz")
+
+	  req, _ := http.NewRequest("GET", getPlaylistsUrl, nil)
+	  req.Header.Set("Authorization", authHeader)
+	  res, _ := httpClient.Do(req)
+	  resBody, _ := ioutil.ReadAll(res.Body)
+
+	  fmt.Fprint(rw, string(resBody))*/
--- models.go
+++ models.go
@@ -5,3 +5,14 @@ type Spotify_User struct {
 	DisplayName string `json:"displayName,Number"`
 	ProfilePic  string `json:"images"`
 }
+
+type Spotify_Auth struct {
+	AccessToken    string `json:"access_token"`
+	TokenType     string `json:"token_type"`
+	ExpiresIn    int   `json:"expires_in"`
+    RefreshToken string `json:"refresh_token"`
+	Scope string `json:"scope"`
+}
+
+
+var Spotify_Auth_Object Spotify_Auth 
