fcdfc1882c6936c0f2edfa531aa16cfa00d9f0d4
Sam Ye <sam.ye@ucla.edu>
2016-11-03 19:58:57 -0700

    basic CreatePartyController and SearchSong tests added

--- api_test.go
+++ api_test.go
@@ -1,33 +1,41 @@
 package main
+// TODO: separate package for testing
 
 import (
     "fmt"
-    "io"
+    //"io"
     "net/http"
     "net/http/httptest"
     "strings"
     "testing"
+    "net/url"
 )
 
 var (
     server   *httptest.Server
-    reader   io.Reader
-    usersUrl string
+    //reader   io.Reader
 )
 
 func init() {
-	//Creating new server with the user handlers
+	// Create server for testing
     server = httptest.NewServer(NewRouter())
-    //Grab the address for the API endpoint
-    usersUrl = fmt.Sprintf("%s/authspotify", server.URL) 
 }
 
+// func TestMain(m *testing.M) {
+// 	fmt.Println("Testing initiated")
+// 	os.Exit(m.Run())
+// }
+
 func TestAuthSpotify(t *testing.T) {
-	reader = strings.NewReader("")
+	fmt.Println("Starting TestAuthSpotify")
+	//t.log("Starting TestAuthSpotify")
 
-	request, err := http.NewRequest("GET", usersUrl, reader)
+	authSpotifyUrl := fmt.Sprintf("%s/authspotify", server.URL)
 
-	res, err := http.DefaultClient.Do(request)
+	reader := strings.NewReader("")
+
+	req, err := http.NewRequest("GET", authSpotifyUrl, reader)
+	res, err := http.DefaultClient.Do(req)
 
 	if err != nil {
 		t.Error(err)
@@ -37,5 +45,65 @@ func TestAuthSpotify(t *testing.T) {
 		t.Errorf("Success expected: %d", res.StatusCode)
 	}
 }
+
+func TestCreatePartyController(t *testing.T) {
+
+	fmt.Println("Starting TestCreatePartyController")
+
+	createPartyUrl := fmt.Sprintf("%s/dashboard/createparty", server.URL)
+
+	form := url.Values{}
+	form.Add("user", "my_user")
+    form.Add("location", "my_location")
+    form.Add("secret-code", "my_code")
+    form.Add("active-time", "my_time")
+
+	reader := strings.NewReader(form.Encode())
+
+	req, err := http.NewRequest("POST", createPartyUrl, reader)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	res, err := http.DefaultClient.Do(req)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if res.StatusCode != 200 {
+		t.Errorf("Success expected: %d", res.StatusCode)
+	}
+
+}
+
+func TestSearchSong(t *testing.T) {
+
+	fmt.Println("Starting TestSearchSong")
+
+	searchSongUrl := fmt.Sprintf("%s/searchsong", server.URL)
+
+	form := url.Values{}
+	form.Add("searchsong", "halo")
+
+	reader := strings.NewReader(form.Encode())
+
+	req, err := http.NewRequest("GET", searchSongUrl, reader)
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	res, err := http.DefaultClient.Do(req)
+
+	if err != nil {
+		t.Error(err)
+	}
+
+	if res.StatusCode != 200 {
+		t.Errorf("Success expected: %d", res.StatusCode)
+	}
+}
+
+
+
+
+
+
+
+
 
 
