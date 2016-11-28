4540c3403cfd3ac3939e53e9630048f5b2bc0791
Brandon Woo <bmw.woo@gmail.com>
2016-11-27 17:54:26 -0800

    Fix css

--- www/js/popup.js
+++ www/js/popup.js
@@ -1,10 +1,4 @@
 // // When the user clicks on <div>, open the popup
-// function togglePopupPartyGoer() {
-//     var popup = document.getElementById('myPopup');
-//     popup.classList.toggle('show');
-//     document.getElementById('usercode').focus();
-// }
-
 function togglePopupPartyGoer() {
   $("#myPopup").load("popup.html", function(){
     $(this).clone().appendTo("body").remove();
